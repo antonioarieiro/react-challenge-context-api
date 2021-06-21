7891d914b6a6a680f7a0ecee3c1a8e6fec24060d
antonioarieiro <sfgfffs@gmail.com>
2021-06-21 12:56:22 -0300

    fix logic 5

--- src/Teste lógico/5.js
+++ src/Teste lógico/5.js
@@ -2,9 +2,10 @@
 depois do array de entrada. Entrada do método ([5,4,3,2,5], 5,3), Resultado do método: [4,2] */
 
 const desafio5 = (num1, num2, num3) => {
-  return num1;
+  const finalRetur = num1.filter((newArray) => newArray !== num2 && newArray !== num3);
+  return finalRetur
 }
 
-console.log(desafio5([1,2,3,4,5],123))
+console.log(desafio5([5,4,3,2,5],5,3))
 console.log(desafio5([1,2,3,4,5],1))
 console.log(desafio5([1,'a'],1))
